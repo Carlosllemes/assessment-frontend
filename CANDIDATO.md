85dcf9485dcd902557328d3095eaabf579ce301d
Carlos Lemes <carlos.lemesxb@gmail.com>
2022-07-04 20:22:05 -0300

    gulp-concat

---
+++ CANDIDATO.md
@@ -0,0 +1,4 @@
+Para por acessar a apliação sem nenhum tipode bug , voce precisa ter o Live Server instalado em seu node. 
+Após abrir o projeto , e clicar em Go live , canto inferior direito na maioria dos casos, acesso o link
+
+http://127.0.0.1:5500/public/index.html
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,16 @@
-const {src, dest, watch} = require('gulp');
+const { src, dest, watch} = require('gulp');
 const sass = require('gulp-sass')(require('sass'))
+const concat = require('gulp-concat')
+
+
 
 function sassCompress (){
     return src('src/**/*.scss')
         .pipe(sass({
             outputStyle:"compressed"
         })).on("error", sass.logError)
-        .pipe(dest('dest/css/'))
+        .pipe(concat('style.css'))
+        .pipe(dest('public/assets/css'))
 }
 
 function watchFiles(){
